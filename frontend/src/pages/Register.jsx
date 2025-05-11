606707f57e490d2113e623f7c27cefee688e21ac
nikhithanikki2004 <nikhithaantony990@gmail.com>
2025-05-11 20:51:10 +0530

    initial commit

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -87,7 +87,7 @@ const Register = () => {
             {loading ? "loading..." :"Sign Up"}
           </button>
           <p className="mt-6 text-center text-sm">
-            Don't have an account?{" "}
+            Already have an account?{" "}
             <Link to={`/login?redirect=${encodeURIComponent(redirect)}`} className="text-blue-500">
               Login
             </Link>
